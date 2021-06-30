6a7b537ad87452f86ab7339b6556815e25b69173
omerhijazi404 <omerhijazi@outlook.com>
2021-06-30 18:03:14 +0300

    Added YELL command

---
+++ AustralianCLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AustralianInterpreter;
+
+namespace AustralianCLI {
+    class Program {
+        static int Main(string[] args) {
+            string file;
+            try {
+                file = args[0];
+            } catch (IndexOutOfRangeException) {
+                Console.WriteLine("No filename provided.");
+                return -1;
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return -1;
+            }
+            Interpreter interpreter = new Interpreter(file);
+            interpreter.Run();
+            Console.WriteLine(interpreter.Output);
+            return 0;
+        }
+    }
+}
---
+++ AustralianInterpreter/Interpreter.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AustralianInterpreter {
+    public class Interpreter {
+        private string[] Code;
+        public string Output;
+
+        public Interpreter(string fileName) {
+            Code = File.ReadAllLines(fileName);
+        }
+
+        public int Run() {
+            int i = 0;
+            bool started = false;
+            foreach (var line in Code) {
+                if (i == 0) {
+                    if (line.ToUpper() == "OI MATE" || line.ToUpper() == "G'DAY COBBA") {
+                        started = true;
+                        continue;
+                    } else {
+                        Output = "Oi donkey, use OI MATE or G'DAY COBBA at the beginning of the code. Kids these days.";
+                        return -1;
+                    }
+                }
+
+                if (started) {
+                    if (line.ToUpper().StartsWith("GET OUT WILL YA")) {
+                        return 0;
+                    } else if (line.ToUpper().StartsWith("YELL")) {
+                        var regex = new Regex(Regex.Escape("YELL"));
+                        var toYell = regex.Replace(line, "", 1).Trim();
+                        Output += "\n" + toYell;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
